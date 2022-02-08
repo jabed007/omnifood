dd9b4f8ee862d34af74350da1d6ef515dbdbf40f
Md. Jabed Hossain <jabed15578@gmail.com>
2022-02-09 00:50:13 +0600

    Implementing Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,55 @@
+///////////////////////////////////////////////////////////
+// Year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
 // Make Mobile Navigation Work
+
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    // Scroll back to Top
+    if (href == "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      // console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
